9a17fa9bdeb18bf886a87d81fe33e443c16b7453
Єгор Непосидяка <Yehor.Neposydiaka@cs.khpi.edu.ua>
2024-10-04 13:22:20 +0300

    Створення бібліотеки классів і створенн двух классів по завданню

---
+++ ClassLib/Department.cs
@@ -0,0 +1,15 @@
+﻿namespace ClassLib
+{
+    public class Department
+    {
+        public int IdDepartment { get; set; }  // Поле для хранения ID департамента
+        public string Name { get; set; }        // Поле для хранения названия департамента
+
+        // Конструктор
+        public Department(int idDepartment, string name)
+        {
+            IdDepartment = idDepartment;
+            Name = name;
+        }
+    }
+}
---
+++ ClassLib/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLib
+{
+    internal class Employee
+    {
+        public int IdEmployee { get; set; }     // Поле для хранения ID сотрудника
+        public string Name { get; set; }         // Поле для хранения имени сотрудника
+        public string Position { get; set; }     // Поле для хранения должности сотрудника
+
+        // Конструктор
+        public Employee(int idEmployee, string name, string position)
+        {
+            IdEmployee = idEmployee;
+            Name = name;
+            Position = position;
+        }
+    }
+}
